5abe9ea11f4016a0e4f6650a9ff3b1d40d83cd47
florlafuente <lafuente.florencia@gmail.com>
2018-08-29 16:04:33 -0300

    Agrega container Proyectos

---
+++ containers/proyectos.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+import { TitleH2, SubtitleH3 } from 'ui'
+
+export default () => (
+  <section>
+    <TitleH2>Proyectos en debate</TitleH2>
+    <SubtitleH3>Estos son los proyectos que podes mirar y comentar para ayudar a hacerlos lo mejor posible.</SubtitleH3>
+  </section>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../containers/navbar'
 import Hero from '../containers/hero'
+import Proyectos from '../containers/proyectos'
 
 export default () => (
   <div>
     <Navbar />
     <Hero />
+    <Proyectos />
   </div>
 )
