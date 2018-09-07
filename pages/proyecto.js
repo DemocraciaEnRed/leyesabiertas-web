0b8e464eeb25a053fdf3256367b8293ad211d052
florlafuente <lafuente.florencia@gmail.com>
2018-09-07 16:50:07 -0300

    Agrega Link en card

---
+++ pages/proyecto.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavBar from '../containers/navbar/component'
+import Footer from '../containers/footer/component'
+
+export default () => (
+  <div>
+    <NavBar />
+    <Footer />
+  </div>
+)
