8531783ab35b7e4b83ee26fce84ca52f4d8b7d63
luarar <laurarh.971@gmail.com>
2018-09-07 14:39:50 -0300

    agrega el container about y sus elementos funcionando

--- elements/about-text/component.js
+++ elements/about-text/component.js
@@ -3,12 +3,15 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const StyledText = styled.div`
-  padding-left: 13rem;
-  padding-top:6.8rem;
-  width: 512px;
+  padding-left: 8%;
+  padding-top: 6.8rem;
+  padding-bottom: 8%;
+  width: 50%;
   height: 540px;
   display:flex;
   flex-direction:column;
+  justify-content:space-between;
+  box-sizing:border-box;
   a {
     text-decoration:none;
     font-size:1.6rem;
