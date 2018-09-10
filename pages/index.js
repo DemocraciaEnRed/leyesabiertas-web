73ed77cd07b03908d7b9ca567ba0b1f8c3d1dec7
luarar <laurarh.971@gmail.com>
2018-09-10 17:48:58 -0300

    fix pages

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,19 @@ import React from 'react'
 import NavBar from '../containers/navbar/component'
 import SecondaryNavbar from '../containers/secondary-navbar/component'
 import Hero from '../containers/hero/component'
+import Projects from '../containers/projects/component'
 import Participate from '../containers/participate/component'
 import About from '../containers/about/component'
 import Footer from '../containers/footer/component'
-import DashboardBar from '../containers/dashboard-bar/component'
 
 export default () => (
   <div>
     <NavBar />
     <SecondaryNavbar />
     <Hero />
+    <Projects />
     <Participate />
     <About />
-    <DashboardBar />
     <Footer />
   </div>
 )
