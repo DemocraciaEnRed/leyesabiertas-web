fdd7206a43869629875e9ccc69581ee9cba0efdf
florlafuente <lafuente.florencia@gmail.com>
2018-10-05 17:40:36 -0300

    Agrega link a articulado

--- components/project-body/component.js
+++ components/project-body/component.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import FundationContainer from '../fundation-container/component'
-import ProjectLinkArticulate from '../../components/project-link'
+import ProjectLinkArticulate from '../../components/project-link-articulate/component'
 
 const value = {
   'object': 'value',
@@ -99,23 +99,8 @@ const BoldP = styled.p`
 
 const ProjectBody = ({ project }) => (
   <ProjectBodyContainer>
-<<<<<<< 6478d54fbaffb93dcee1d608b61f8b2797e2f22a
-    <H2>Porque proponemos una Ley de Acceso a la información Pública</H2>
-    <BoldP>ARTÍCULO 1 - </BoldP>
-    <P>Kinfolk mlkshk pork belly normcore twee tattooed next level pinterest farm-to-table. Dreamcatcher intelligentsia kinfolk ramps swag next level disrupt, cred kale chips locavore XOXO before they sold out subway tile. Humblebrag lomo mixtape coloring book truffaut meditation, helvetica street art microdosing. Tattooed microdosing skateboard helvetica tumblr ennui.
-
-Dreamcatcher air plant chambray VHS kickstarter. Fingerstache meggings vexillologist gentrify seitan. Wayfarers sriracha master cleanse edison bulb, actually cray tousled pok pok cardigan beard craft beer letterpress af. Cardigan adaptogen vegan ramps godard aesthetic plaid schlitz post-ironic enamel pin chicharrones church-key. Kombucha disrupt stumptown coloring book banjo vegan listicle small batch. Vegan truffaut dreamcatcher banh mi VHS la croix quinoa microdosing. Banjo 8-bit viral distillery four dollar toast readymade slow-carb narwhal scenester vape.
-
-Vape wayfarers locavore, blue bottle keytar mumblecore hell of post-ironic listicle 90's williamsburg tousled you probably haven't heard of them roof party. XOXO vegan lyft activated charcoal paleo. Blue bottle tote bag farm-to-table, vegan microdosing scenester ennui. Subway tile ramps next level knausgaard hot chicken tattooed craft beer. Seitan sartorial meggings selvage, occupy stumptown PBR&B. Flexitarian 90's helvetica typewriter, raw denim seitan squid hell of listicle master cleanse dreamcatcher.
-
-Bespoke tumeric sartorial heirloom blue bottle. Plaid tumeric pickled, vape selvage cloud bread kogi taiyaki vaporware. Farm-to-table paleo gochujang venmo shabby chic literally tumblr, cray offal tbh la croix wolf jianbing vexillologist godard. Chartreuse cold-pressed pabst, letterpress subway tile hashtag vaporware tilde jean shorts la croix PBR&B celiac gochujang tumeric deep v. Semiotics aesthetic hell of tacos marfa before they sold out, fashion axe banh mi iceland four loko pabst neutra XOXO biodiesel tumblr. Helvetica food truck banjo celiac, sartorial try-hard raw denim tacos yuccie.
-
-Meditation green juice subway tile franzen skateboard next level. Venmo edison bulb before they sold out waistcoat hexagon meh selvage shaman mlkshk. Af air plant next level, try-hard pitchfork activated charcoal banjo 3 wolf moon cornhole cred locavore cliche lomo squid flannel. Aesthetic chartreuse church-key vice kogi meggings. Schlitz coloring book mlkshk mumblecore live-edge scenester meggings knausgaard everyday carry.</P>
-    <ProjectLinkArticulate />
-
-=======
     <FundationContainer value={value} />
->>>>>>> Agrega slate para ver los fundamentos del proyecto
+    <ProjectLinkArticulate id={project.id} />
   </ProjectBodyContainer>
 )
 
