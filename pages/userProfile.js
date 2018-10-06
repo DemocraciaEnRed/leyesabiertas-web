38dc000bc81f429f5ca3bcb506c115be2a9b98ca
Pablo Mendez <mendez.developer@gmail.com>
2018-10-06 11:41:36 -0300

    Revert "Fixed unexistent component"
    This reverts commit 9409adbed0e4a3dbd9391fdf6cfb213ce6a2802a.

--- pages/userProfile.js
+++ pages/userProfile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import NavBar from '../containers/navbar/component'
+import UserProfileContainer from '../containers/user-profile-container/component'
 import Footer from '../containers/footer/component'
 
 class UserProfile extends Component {
@@ -12,6 +13,7 @@ class UserProfile extends Component {
     return (
       <div>
         <NavBar />
+        <UserProfileContainer project={this.props.id} />
         <Footer />
       </div>
     )
