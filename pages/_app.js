a3429a0e19ddd1e6d1088c53c4d4aec550543a39
florlafuente <lafuente.florencia@gmail.com>
2018-10-11 14:37:54 -0300

    Add keycloak in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { Context } from 'react'
 import App, { Container } from 'next/app'
+const json = import('../json/keycloak.json')
+let Keycloak
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
+  static async getInitialProps ({ Component, router, ctx }) {
     let pageProps = {}
-
+    let keycloak = await json
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx)
     }
 
-    return { pageProps }
+    return { pageProps, keycloak }
+  }
+
+  state = {
+    keycloak: null,
+    authenticated: false
+  }
+
+  async componentDidMount () {
+    Keycloak = require('keycloak-js')
+    const keycloak = await Keycloak(this.props.keycloak)
+    try {
+      const authenticated = await keycloak.init({ onLoad: 'check-sso' })
+      this.setState({
+        keycloak, authenticated
+      })
+    } catch (err) {
+      console.error(err)
+      this.setState({
+        authenticated: false
+      })
+    }
   }
 
   render () {
