e714d354d6bb8cff9e7322725b5f55ada2fa40fc
florlafuente <lafuente.florencia@gmail.com>
2018-10-12 14:17:42 -0300

    Agrega funcion login

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,8 @@ export default class MyApp extends App {
 
   state = {
     keycloak: null,
-    authenticated: false
+    authenticated: false,
+    login: null
   }
 
   async componentDidMount () {
@@ -26,7 +27,9 @@ export default class MyApp extends App {
     try {
       const authenticated = await keycloak.init({ onLoad: 'check-sso' })
       this.setState({
-        keycloak, authenticated
+        keycloak: keycloak,
+        authenticated: authenticated,
+        login: keycloak.login
       })
     } catch (err) {
       console.error(err)
