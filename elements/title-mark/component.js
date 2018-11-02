a1e412475ff88766cafb661784da2b7d2b950af9
Guillermo Croppi <guillermocroppi@gmail.com>
2018-11-02 15:42:46 -0300

    Style fix

--- elements/title-mark/component.js
+++ elements/title-mark/component.js
@@ -2,10 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledTitleMark = styled.h2`
-  font-size: 1.8rem;
+  font-size: 2rem;
   font-family: var(--bold);
   line-height: 1.89;
   color: #203340;
+  margin-top: 30px;
   margin-bottom: 30px;
 `
 
