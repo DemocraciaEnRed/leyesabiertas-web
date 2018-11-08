c63fe1755ecdc1d2451bc8bbaa487e29ca2b532f
luarar <laurarh.971@gmail.com>
2018-11-08 16:34:08 -0300

    agrega alert luego de enviar el comentario

--- components/project-text-create-comment/comment-form.js
+++ components/project-text-create-comment/comment-form.js
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types'
 import styled, { consolidateStreamedStyles } from 'styled-components'
 import fetch from 'isomorphic-unfetch'
 import { Mark } from 'slate'
-import WithUserContext from '../../components/with-user-context/component'
+import Icon from 'react-icons-kit'
+import { checkCircleO } from 'react-icons-kit/fa/checkCircleO'
+import { timesCircleO } from 'react-icons-kit/fa/timesCircleO'
 import UserAvatarLogged from '../../elements/user-avatar-logged/component'
+import WithUserContext from '../../components/with-user-context/component'
 
 const API_URL = process.env.API_URL
 
@@ -22,24 +25,22 @@ const CommentFormContainer = styled.form`
   cursor: pointer;
   z-index:2;
   position: absolute;
-  left: 70%;
+  left: 74%;
 `
 
 const CommentStatus = styled.form`
   width: 300px;
-  min-height: 305px;
-  border-radius: 3px;
+  height: 122px;
   box-shadow: 0 4px 20px 0 rgba(0, 0, 0, 0.05);
+  border: solid 1px ${(props) => props.error ? '#ea5f5f' : '#b8e986'};
   background-color: #ffffff;
-  border: solid 1px #dae1e7;
   display:flex;
-  flex-direction:column;
   justify-content:space-between;
   box-sizing: border-box;
   cursor: pointer;
   z-index:2;
   position: absolute;
-  left: 70%;
+  left: 74%;
 `
 
 const CommentFormContent = styled.div`
@@ -94,12 +95,40 @@ const CommentText = styled.textarea`
   border-style: none; 
   border-color: Transparent; 
 `
+
+const IconDiv = styled.div`
+  width: 50px;
+  background-color: ${(error) => error ? '#ea5f5f' : '#b8e986'};
+  color: #fff;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`
+const TextTitle = styled.div`
+  color: #2c4c61;
+  font-size:1.4rem;
+  margin-bottom:1.6rem;
+  font-family:var(--bold);
+`
+const TextDiv = styled.div`
+  display:flex;
+  flex-direction:column;
+  padding-left:1rem;
+  justify-content:center;
+
+`
+const Text = styled.div`
+  color: #181818;
+  font-size:1.4rem;
+`
+
 class CommentForm extends Component {
   constructor (props) {
     super(props)
     this.state = {
       value: '',
-      status: false
+      status: false,
+      error: false
     }
 
     this.handleChange = this.handleChange.bind(this)
@@ -132,8 +161,6 @@ class CommentForm extends Component {
         decoration.mark.data.preview = false
         const decorations = this.props.editor.value.decorations.push(decoration)
         this.props.editor.setDecorations(decorations)
-        console.log('Saved on DB...')
-        this.setCommentId(res)
         this.setState({
           value: '',
           status: true
@@ -141,6 +168,9 @@ class CommentForm extends Component {
       })
       .catch((err) => {
         console.log(err)
+        this.setState({
+          error: true
+        })
       })
   }
 
@@ -161,8 +191,15 @@ class CommentForm extends Component {
             </CommentFormContent>
             <CommentFormFooter onClick={this.handleSubmit}>Enviar comentario</CommentFormFooter>
           </CommentFormContainer>
-          
-          : <CommentStatus>Envio comentario</CommentStatus>
+
+          : <CommentStatus style={{ top: this.props.top }} color={{ error: this.state.error }}>
+            {!this.state.error ? <IconDiv><Icon icon={checkCircleO} /></IconDiv>
+              : <IconDiv error><Icon icon={timesCircleO} /></IconDiv> }
+            <TextDiv>
+              <TextTitle>{!this.state.error ? 'Gracias por tu aporte' : 'Ha ocurrido un error'}</TextTitle>
+              <Text>{!this.state.error ? 'Su comentario ha sido enviado al diputado y sus asesores.' : 'Lo sentimos. Por favor intente nuevamente más tarde.' }</Text>
+            </TextDiv>
+          </CommentStatus>
         }
       </Fragment>
     )
