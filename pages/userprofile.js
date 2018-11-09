da9f1fae5d18052ee52064be1c2fcacf08921258
luarar <laurarh.971@gmail.com>
2018-11-09 17:59:55 -0300

    mejora ubicacion de footer al cargar

--- pages/userprofile.js
+++ pages/userprofile.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import NavBar from '../containers/navbar/component'
 import SecondaryNavbar from '../containers/secondary-navbar/component'
 import UserProfileContainer from '../containers/user-profile/component'
 import Footer from '../containers/footer/component'
+
+const Wrapper = styled.div`
+  display:flex;
+  flex-direction:column;
+  justify-content:space-between;
+  min-height:100vh;
+`
 
 class UserProfile extends Component {
   static getInitialProps ({ query: { id } }) {
@@ -12,12 +20,14 @@ class UserProfile extends Component {
 
   render () {
     return (
-      <div>
-        <NavBar />
-        <SecondaryNavbar />
-        <UserProfileContainer userId={this.props.id} />
+      <Wrapper>
+        <div>
+          <NavBar />
+          <SecondaryNavbar />
+          <UserProfileContainer userId={this.props.id} />
+        </div>
         <Footer />
-      </div>
+      </Wrapper>
     )
   }
 }
