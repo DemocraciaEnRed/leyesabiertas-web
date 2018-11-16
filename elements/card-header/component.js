516ef9701ed90f9613616418b107cbb47ed1dcaf
Guillermo Croppi <guillermocroppi@gmail.com>
2018-11-16 15:05:57 -0300

    Agregado label de "Oculto" en las cards de proyecto

--- elements/card-header/component.js
+++ elements/card-header/component.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-
+import Icon from 'react-icons-kit/Icon'
+import { eyeSlash } from 'react-icons-kit/fa/eyeSlash'
 const Wrapper = styled.div`
 width: 368px;
 height: 126px;
@@ -10,12 +11,36 @@ background-image: url('${(props) => props.img}');
 overflow:hidden;
 background-size: cover;
 background-position: center;
+// position: relative;
 `
-const CardHeader = ({ img }) => (
-  <Wrapper img={img} />
+
+const Label = styled.span`
+  position: absolute;
+  top: 13px;
+  left: 10px;
+  width: 95px;
+  height: 20px;
+  border-radius: 4px;
+  background-color: #fee282;
+  font-size: 1.0rem;
+  font-weight: 500;
+  color: #000;
+  padding: 5px 8px;
+  text-transform: uppercase;
+  align-self: flex-end;
+  margin-bottom: 8px;
+  position: relative;
+  bottom: 7px
+`
+
+const CardHeader = ({ img, published }) => (
+  <Wrapper img={img}>
+    { !published && <Label><Icon icon={eyeSlash} style={{ verticalAlign: 'text-bottom', marginRight: '5px' }} size={14} />Oculto</Label>}
+  </Wrapper>
 )
 
 CardHeader.propTypes = {
-  img: PropTypes.string
+  img: PropTypes.string,
+  published: PropTypes.bool
 }
 export default CardHeader
