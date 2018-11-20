2b26e4bdf55ac9d5b0bfabe6e9b7d0e3f78131a7
florlafuente <lafuente.florencia@gmail.com>
2018-11-20 10:29:26 -0300

    Agrega componente tooltip

---
+++ components/tooltip/component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+class Tooltip extends Component {
+  render () {
+    return (
+      <div>
+        Soy un tooltip
+      </div>
+    )
+  }
+}
+
+export default Tooltip
