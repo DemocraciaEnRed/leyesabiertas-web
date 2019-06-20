bf9df7e144a006a786eecc8bc0e717a90cb58748
Guillermo Croppi <guillermocroppi@gmail.com>
2019-06-20 16:30:53 -0300

    DERLA-60 Agregado el video tutorial al portal en la home

--- containers/hero/component.js
+++ containers/hero/component.js
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import HeroTitle from '../../elements/hero-title/component'
 import HeroSubtitle from '../../elements/hero-subtitle/component'
+import HeroVideo from '../../elements/hero-video/component'
 
 const StyledHero = styled.div`
-  height: 350px;
+  min-height: 350px;
   background-image: url('/static/assets/background-hero.jpg');
   background-repeat: no-repeat;
     background-position: right 0px bottom 42%;
@@ -15,7 +16,7 @@ const StyledHero = styled.div`
   align-items: flex-start;
   justify-content: center;
   padding: 0 7rem;
-  @media (max-width: 700px) {
+  @media (max-width: 1060px) {
     background-position: 46% center;
     background-size: cover;
   }
@@ -25,6 +26,7 @@ const Hero = () => (
   <StyledHero>
     <HeroTitle>Plataforma de Participación Ciudadana en Propuestas de Ley</HeroTitle>
     <HeroSubtitle>¡Participe haciendo comentarios y sugerencias para co-crear mejores leyes!</HeroSubtitle>
+    <HeroVideo youtubeId='r9hZuc72C48' />
   </StyledHero>
 )
 
