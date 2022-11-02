f56a7ed5f624f2abaa15c259a231fd39b1cd1551
xtian7489 <xtian7489@gmail.com>
2022-11-02 11:49:37 -0300

    filtros por diputado, diputada, bloque y titulo de propuesta

--- containers/projects/component.js
+++ containers/projects/component.js
@@ -17,12 +17,11 @@ const { publicRuntimeConfig: { API_URL } } = getConfig()
 
 const masonryOptions = {
   transitionDuration: 0
-};
-
+}
 
 const OptionChoice = styled.div`
 display: inline-block;
-margin: 0 5px;
+margin: 5px;
 font-size: 1.4rem;
 padding: 5px 8px;
 border-radius: 4px;
@@ -44,15 +43,29 @@ color: #2c4c61;
   border-color: #777;
 }
 `
-const Options = styled.div`
 
+const OptionsWrapper = styled.div`
+position:relative;
+`
+
+const OptionsSection = styled.div`
+padding-left: 5px
+`
+const Options = styled.div`
+width:70%;
+display:flex;
+@media(max-width:700px){
+  width:100%;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+ }
 `
 const OptionLabel = styled.div`
 font-size: 1.4rem;
 color: #2c4c61;
-display: inline-block;
-padding: 5px 8px;
+padding: ${(props) => props.isTitle ? '8px 0' : '5px 8px'};
 font-weight: ${(props) => props.isTitle ? 'bold' : 'normal'};
+display: ${(props) => props.isTitle ? 'block' : 'inline-block'};
 &:first-child{
   margin-left: 0;
   padding-left: 0;
@@ -96,6 +109,78 @@ text-align: center;
 width: 100%;
 `
 
+const FilterButton = styled.button`
+margin-left:25px;
+border: none;
+border-radius:5px;
+padding: 10px 20px;
+font-size: 1.4rem;
+color: #FFF;
+background-color: #5c97bc;
+font-family: var(--bold);
+cursor: pointer;
+@media(max-width:700px){
+  margin-top: 8px;
+  color: #5c97bc;
+  background-color:#fff0;
+ }
+`
+const Icon = styled.div`
+  width: 18px;
+  height: 15px;
+  background-image: url(${(props) => `/static/assets/${props.icon}`});
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: inline-block;
+  position: relative;
+  top: 2px;
+  @media(max-width:700px){
+filter:grayscale(100%) brightness(54%) sepia(100%) hue-rotate(-180deg) saturate(700%) contrast(0.8);
+}
+`
+
+const Search = styled.input`
+display: inline-block;
+margin: 0 5px;
+font-size: 1.5rem;
+padding: 8px;
+border-radius: 4px;
+border: 1px solid #4C4C4E;
+width:75%;
+::placeholder{
+  font-size:1.5rem;
+}
+&.disabled{
+  color: #777;
+  border-color: #777;
+}
+@media(max-width:700px){
+  display: block;
+  width:100%;
+ }
+`
+const Filters = styled.div`
+background:white;
+position: absolute;
+right: -195px;
+top: 50px;
+z-index: 999;
+padding: 15px
+border-radius:5px
+box-shadow: 0px 3px 4px 0px #9999996b;
+@media(max-width:700px){
+  right: 0;
+  left: -185px;  
+}
+`
+
+let delay = (function () {
+  let timer = 0
+  return function (callback, ms) {
+    clearTimeout(timer)
+    timer = setTimeout(callback, ms)
+  }
+})()
 class Projects extends Component {
 
   constructor(props) {
@@ -105,14 +190,16 @@ class Projects extends Component {
       projectsFiltered: [],
       loadMoreAvailable: false,
       loading: true,
+      filterShow:false,
       // page: 1,
       // noMore: false,
       query: {
-        created: 'DESC',
+        created: 'SUPP',
         limit: 10,
         page: 1,
         closed: null,
-        tag: null
+        tag: null,
+        textFilter: null
       },
       // filter: {
       //   closed: null
@@ -134,7 +221,9 @@ class Projects extends Component {
     try {
       this.setState({
         loading: true
-      }, this.fetchDocuments)
+      }, delay(() => {
+        this.fetchDocuments()
+      }, 500))
     } catch (error) {
       console.error(error)
     }
@@ -143,9 +232,9 @@ class Projects extends Component {
   async fetchDocuments() {
     let tag = this.props.router.query.tag;
     let currentQuery = {...this.state.query};
-    currentQuery.tag = tag;
+    currentQuery.tag = tag
     
-    let query = this.createQuery(currentQuery);
+    let query = this.createQuery(currentQuery)
     
     const projects = await (await fetch(`${API_URL}/api/v1/documents${query}`)).json()
     // let mergedProjects = this.state.projects.concat(projects.results)
@@ -179,9 +268,14 @@ class Projects extends Component {
     })
   }
 
-
   async componentDidMount() {
     this.getDocuments()
+  }
+
+  handleShowFilters = () => {
+    this.setState({
+      filterShow: !this.state.filterShow
+    })
   }
 
   toggleSort = (parameter, value) => {
@@ -194,11 +288,15 @@ class Projects extends Component {
     }, () => {
       this.getDocuments()
     })
-    let tag = newQuery["tag"]
-    if(tag){
+    let tag = newQuery['tag']
+    if (tag) {
       router.push({
         pathname: this.props.router.pathname,
-        query: {tag}
+        query: { tag }
+      })
+    } else {
+      router.push({
+        pathname: this.props.router.pathname
       })
     }
   }
@@ -230,17 +328,29 @@ class Projects extends Component {
         <TitleH2>Propuestas de ley</TitleH2>
         <SubtitleH3>Acá podés acceder a las propuestas de ley para leerlas, apoyarlas y hacer tus aportes. ¡Ayudanos a mejorarlas!</SubtitleH3>
         <Options>
-          <OptionLabel isTitle>Ordenar:</OptionLabel>
-          {query.created === 'ASC' && <OptionChoice onClick={() => this.toggleSort('created', 'DESC')}>Más antiguas</OptionChoice>}
-          {query.created === 'DESC' && <OptionChoice onClick={() => this.toggleSort('created', 'ASC')}>Más recientes</OptionChoice>}
-          <OptionLabel></OptionLabel>
-          <OptionLabel isTitle>Filtrar:</OptionLabel>
-          <OptionLabel>Por estado</OptionLabel>
-          {query.closed === null && <OptionChoice onClick={() => this.toggleSort('closed', true)}>TODOS</OptionChoice>}
-          {query.closed === true && <OptionChoice onClick={() => this.toggleSort('closed', false)}>FINALIZADOS</OptionChoice>}
-          {query.closed === false && <OptionChoice onClick={() => this.toggleSort('closed', null)}>ABIERTOS</OptionChoice>}
-          <OptionLabel>Por etiqueta</OptionLabel> 
-          {tags.length > 0 && <TagsSelect allTags={tags} selected={query.tag} onTagChange={(tagId) => this.toggleSort('tag', tagId)} />}
+          <Search type='text' placeholder='Buscá por nombre de la Diputada o Diputado o propuesta' onInput={(e) => this.toggleSort('textFilter', e.target.value)} />
+          <OptionsWrapper>
+            <FilterButton onClick={this.handleShowFilters}>Filtrar <Icon icon='down-arrow.svg' /></FilterButton>
+            <Filters style={{ display: this.state.filterShow ? 'block' : 'none' }}>
+              <OptionsSection>
+                <OptionLabel>Ordenar:</OptionLabel>
+                {query.created === 'ASC' && <OptionChoice onClick={() => this.toggleSort('created', 'DESC')}> Más antiguas</OptionChoice>}
+                {query.created === 'SUPP' && <OptionChoice onClick={() => this.toggleSort('created', 'ASC')}> Más recientes</OptionChoice>}
+                {query.created === 'DESC' && <OptionChoice onClick={() => this.toggleSort('created', 'SUPP')}> Más apoyos</OptionChoice>}
+              </OptionsSection>
+              <OptionLabel isTitle>Filtrar por:</OptionLabel>
+              <OptionsSection>
+                <OptionLabel>Estado:</OptionLabel>
+                {query.closed === null && <OptionChoice onClick={() => this.toggleSort('closed', true)}>TODOS</OptionChoice>}
+                {query.closed === true && <OptionChoice onClick={() => this.toggleSort('closed', false)}>FINALIZADOS</OptionChoice>}
+                {query.closed === false && <OptionChoice onClick={() => this.toggleSort('closed', null)}>ABIERTOS</OptionChoice>}
+              </OptionsSection>
+              <OptionsSection>
+                <OptionLabel>Etiqueta</OptionLabel>
+                {tags.length > 0 && <TagsSelect allTags={tags} selected={query.tag} onTagChange={(tagId) => this.toggleSort('tag', tagId)} />}
+              </OptionsSection>
+            </Filters>
+          </OptionsWrapper>
         </Options>
         {projects &&
           <Fragment>
